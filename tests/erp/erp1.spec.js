1e600ed4ec23f8d24f002e282b38c092edfe782c
KavyaNsrinivasa <kavyasrinivasa1@gmail.com>
2025-12-27 13:28:09 +0530

    modified

--- tests/erp/erp1.spec.js
+++ tests/erp/erp1.spec.js
@@ -4,7 +4,7 @@ import erplanding from "../../PageObjectModel/erp/ERPlanding.page"
 import adminhome from "../../PageObjectModel/erp/erpadminhome.page"
 import customerlist from "../../PageObjectModel/erp/erpcustomerlist.page"
 import custpage from "../../PageObjectModel/erp/erpaddcust.page"
-import custdata from "../../testData/erpdata/erpdata.json"
+import custdata from "../../testData/erpdata/erpcustdata.json"
 import editcustpage from "../../PageObjectModel/erp/erpeditcust.page"
 
 test("scenario1", async({page})=>{
@@ -51,43 +51,43 @@ test("scenario1", async({page})=>{
     //click save
     await addcust.savebtn.click()
     //click on profile dropdown
-    await customer.profiledropdown.click()
+    await home.profiledropdown.click()
     //click on logout
-    await customer.logoutlink.click()
+    await home.logoutlink.click()
 })
 
-test("scenario2", async({page})=>{
-    page.on('dialog',async (dialog)=>{
-        console.log(await dialog.message)
-        await dialog.accept()
-    })
-    let url = erpdata.url
-    let adminusn = erpdata.username
-    let adminpwd = erpdata.password
-    let custf = custdata.custfn
-    let landing = new erplanding(page);
-    let home = new adminhome(page)
-    let customer = new customerlist(page)
-    let editc = new editcustpage(page)
-    //launch url
-    await page.goto(url)
-    //click on admin tab
-    await landing.admintab.click()
-    //enter username
-    await expect(landing.adminusernametf).toBeEnabled
-    await landing.adminusernametf.fill(adminusn)
-    //enter password
-    await landing.adminpasswordtf.fill(adminpwd)
-    //click on login
-    await landing.adminloginbutton.click()
-    //in home page click on customer
-    await home.customerlink.click()
-    //update a user and save
-    await customer.editbtn.click()
-    await editc.fname.fill(custf)
-    await editc.updatebutton.click()
-    //click on profile dropdown
-    await customer.profiledropdown.click()
-    //click on logout
-    await customer.logoutlink.click()
-})
+// test("scenario2", async({page})=>{
+//     page.on('dialog',async (dialog)=>{
+//         console.log(await dialog.message)
+//         await dialog.accept()
+//     })
+//     let url = erpdata.url
+//     let adminusn = erpdata.username
+//     let adminpwd = erpdata.password
+//     let custf = custdata.custfn
+//     let landing = new erplanding(page);
+//     let home = new adminhome(page)
+//     let customer = new customerlist(page)
+//     let editc = new editcustpage(page)
+//     //launch url
+//     await page.goto(url)
+//     //click on admin tab
+//     await landing.admintab.click()
+//     //enter username
+//     await expect(landing.adminusernametf).toBeEnabled
+//     await landing.adminusernametf.fill(adminusn)
+//     //enter password
+//     await landing.adminpasswordtf.fill(adminpwd)
+//     //click on login
+//     await landing.adminloginbutton.click()
+//     //in home page click on customer
+//     await home.customerlink.click()
+//     //update a user and save
+//     await customer.editbtn.click()
+//     await editc.fname.fill(custf)
+//     await editc.updatebutton.click()
+//     //click on profile dropdown
+//     await home.profiledropdown.click()
+//     //click on logout
+//     await home.logoutlink.click()
+// })
